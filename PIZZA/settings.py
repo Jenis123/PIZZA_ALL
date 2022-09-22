c9d283666c296633d1ce64ec6bdad4ace6dd7b8f
Jenis123 <98075956+Jenis123@users.noreply.github.com>
2022-09-22 15:45:42 +0530

    Update settings.py

--- PIZZA/settings.py
+++ PIZZA/settings.py
@@ -134,8 +134,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT=os.path.join(BASE_DIR,  'staticfiles')
-STATICFILES_DIRS= (os.path.join(BASE_DIR,'static'), )
 
 
 STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
